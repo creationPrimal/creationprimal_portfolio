0fdef9b04039156f7ec87f8abd7cedf1c2447e42
Creation Primal <creationprimal@gmail.com>
2025-07-25 16:23:42 +0300

    new changes

--- assets/invoice_generator/js/app.js
+++ assets/invoice_generator/js/app.js
@@ -323,15 +323,14 @@ document.addEventListener("DOMContentLoaded",() => {
 
 function downloadInvoicePDF() {
   const element = document.getElementById('invoice');
-  const invoicePreview = document.getElementById('invoicePreview');
+//   const invoicePreview = document.getElementById('invoicePreview');
   const invoiceName = document.getElementById('invoiceName').value || "";
 
-  const originalDisplay = element.style.display;
 
   //element.style.width = '794px';
   element.style.padding = '40px'; // Adjust for nice inner spacing
-  invoicePreview.style.display = 'flex'; // show the preview div so to capture data
-  invoicePreview.style.left = '-9999px';
+//   invoicePreview.style.display = 'flex'; // show the preview div so to capture data
+//   invoicePreview.style.left = '-9999px';
 
   const options = {
     margin: 0,
@@ -350,7 +349,7 @@ function downloadInvoicePDF() {
   };
 
   html2pdf().set(options).from(element).save();
-  invoicePreview.style.display = 'none'; // remove the preview btn
+//   invoicePreview.style.display = 'none'; // remove the preview btn
   // alert user
   alert('Your Invoice has been Created successfully, Press "Ok" to download it. Thank you for using our tool. 😊🫶❤️')
 }
